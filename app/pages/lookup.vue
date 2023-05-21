96b214085105c017e54c33cc93b6c65bc9ad8c4f
anon <anon@anoncom.net>
2023-05-21 23:49:21 +0900

    Refactor input handling or DID

--- app/pages/lookup.vue
+++ app/pages/lookup.vue
@@ -7,7 +7,7 @@
           </div>
           <div class="flex items-center bg-gray-200 rounded-md">
             <div class="w-full p-2">
-              <input class="bg-transparent rounded-md w-full text-gray-700" v-model="inputHandle" placeholder="jack.bsky.social" />
+              <input class="bg-transparent rounded-md w-full text-gray-700" v-model="handle" @keyup.enter="requestDID" placeholder="jack.bsky.social" />
             </div>
             <div class="p-2">
               <button class="bg-blue-500 text-white rounded-md px-2 py-1" @click="requestDID">Lookup</button>
@@ -29,33 +29,40 @@
 </template>
 
 <script>
-  import axios from 'axios';
+  import axios from 'axios'
+  import { ref } from 'vue'
+  import { useRoute } from 'vue-router'
   import { isDev } from '~/utils'
 
   const defaultDomain = '.bsky.social' 
 
   export default {
     layout: 'default',
+    setup() {
+      const route = useRoute();
+      const handle = ref(route.query.handle || '');
+      return { handle }
+    },
     data() {
       return {
-        inputHandle: '',
         result: '',
         hasError: false
       }
     },
     methods: {
-      
       async requestDID() {
         try {
-          let text = this.inputHandle;
+          let text = this.handle;
           if (!text.startsWith('did:')) {
             text.startsWith('@') ? text.substring(1) : text
             text.startsWith('at://') ? text.substring(5) : text
-            this.inputHandle = text
-            if (!text.includes('.'))
+            this.handle = text
+            if (!text.includes('.')) {
               text = text + defaultDomain // default xxx -> xxx.bsky.social
-              this.inputHandle = text
+              this.handle = text
+            }
           }
+          this.$router.push({ query: { handle: text } });
 
           let requestUrl;
           if (text.startsWith('did:')) 
