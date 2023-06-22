92b3dcbb0999eeab8bb0efd753289e5c0fe73ecf
anon <anon@anoncom.net>
2023-06-22 20:48:30 +0900

    Add app icons (#6)
    * Add icons
    * Specify the icons

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -11,6 +11,81 @@ export default {
         {
           name: 'viewport',
           content: 'width=device-width, initial-scale=1',
+          description: 'SkyTools is a tools for the Bluesky',
+        },
+        {
+          name: 'theme-color',
+          content: '#ccf2ff',
+        },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '180x180',
+          href: '/icons/apple-touch-icon.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '144x144',
+          href: '/icons/apple-touch-icon-144x144.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '96x96',
+          href: '/icons/apple-touch-icon-96x96.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '72x72',
+          href: '/icons/apple-touch-icon-72x72.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          type: 'image/png',
+          sizes: '57x57',
+          href: '/icons/apple-touch-icon-57x57.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '180x180',
+          href: '/icons/icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '144x144',
+          href: '/icons/icon-144x144.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '96x96',
+          href: '/icons/icon-96x96.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '72x72',
+          href: '/icons/icon-72x72.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '57x57',
+          href: '/icons/icon-57x57.png',
+        },
+      ],
+      script: [
+        {
+          src: 'https://static.cloudflareinsights.com/beacon.min.js',
+          defer: true,
+          data_cf_beacon: process.env.CLOUDFLARE_BEACON_KEY ?? '{"token":""}',
         },
       ],
     },
