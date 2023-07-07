7531526d1e7b2b81afad54d6624929e8fc1de60a
anon <anon@anoncom.net>
2023-07-08 02:52:00 +0900

    Rewrite history page nuxt3 style

--- app/pages/history.vue
+++ app/pages/history.vue
@@ -63,100 +63,102 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { DateTime } from 'luxon'
-import axios from 'axios'
 import { isDev } from '@/utils/helpers'
-import { formatIdentifier, resolveHandle, getIdentityAuditLogs } from '~/utils/lexicons'
+import { formatIdentifier, resolveHandle, getIdentityAuditLogs } from '@/utils/lexicons'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+import axios from 'axios'
 
-export default {
-    layout: 'default',
-    data() {
-        return {
-        results: [],
-            hasError: false,
+const route = useRoute()
+const router = useRouter()
+
+const handle = ref(route.query.id || '')
+const results = ref([])
+const hasError = ref(false)
+
+onMounted(() => {
+  if (route.query.id)
+    handle.value = route.query.id
+
+  if (handle.value.length > 0)
+    getHistory(handle.value)
+})
+
+const focusout = () => {
+  handle.value = formatIdentifier(handle.value)
+}
+
+const submit = () => {
+  router.push({ query: { id: handle.value } })
+  if (handle.value.length > 0) {
+    getHistory(handle.value)
+  }
+}
+
+/** Get DID from handle */
+const getDID = async handle => {
+  try {
+    hasError.value = false
+    handle = formatIdentifier(handle)
+    router.push({ query: { id: handle } })
+    return await resolveHandle(handle)
+  } catch (error) {
+    hasError.value = true
+    if (isDev()) console.error(error)
+
+    if (axios.isAxiosError(error) && error.response.data.message)
+      results.value = error.response.data.message
+    else
+      results.value = error.message
+  }
+}
+  /** Get handle history from DID */
+const getHistory = async did => {
+  try {
+    if (!did.startsWith('did:')) did = await getDID(did)
+    if (hasError.value) return
+
+    const res = await getIdentityAuditLogs(did)
+
+    if (res.length > 0) {
+      let records = res.reverse()
+      let items = []
+      for (let idx in records) {
+        let record, icon, style
+        record = records[idx]
+        icon = ['fas', 'square-minus']
+        style = { color: '#cccccc' }
+      if (idx == 0) {
+        icon = ['fas', 'circle-check']
+        style = { color: '#18b404' }
+      } else if (
+        record.operation?.type === 'create'
+        || record.operation?.prev === null
+      ) {
+        icon = ['fas', 'flag']
+        style = { color: '#ea2a63' }
       }
-    },
-    setup() {
-      const route = useRoute()
-      const handle = ref(route.query.handle || '')
-      return { handle }
-    },
-    methods: {
-      focusout() {
-        this.handle = formatIdentifier(this.handle)
-        },
-        submit() {
-        this.$router.push({ query: { handle: this.handle } })
-          if (this.handle.length > 0) {
-            this.getHistory(this.handle)
-          }
-        },
-        /** Get DID from handle */
-        async getDID(handle) {
-        try {
-          this.hasError = false
-          handle = formatIdentifier(handle)
-          this.id = handle
-          this.$router.push({ query: { id: handle } })
-          return await resolveHandle(handle)
-        } catch (error) {
-          this.hasError = true
-          if (isDev()) console.error(error)
-            this.results = error.message
-          if (error.response.data.message)
-            this.results = error.response.data.message
-        }
-        },
-        /** Get handle history from DID */
-        async getHistory(did) {
-        try {
-          if (!did.startsWith('did:')) did = await this.getDID(did)
-          if (this.hasError) return
-
-          const res = await getIdentityAuditLogs(did)
-
-          if (res.length > 0) {
-            let records = res.reverse()
-            let items = []
-            for (let idx in records) {
-              let record, icon, style
-              record = records[idx]
-              icon = ['fas', 'square-minus']
-              style = { color: '#cccccc' }
-            if (idx == 0) {
-              icon = ['fas', 'circle-check']
-              style = { color: '#18b404' }
-            } else if (
-              record.operation?.type === 'create'
-              || record.operation?.prev === null
-            ) {
-              icon = ['fas', 'flag']
-              style = { color: '#ea2a63' }
-            }
-            items.push({
-              id: record.cid,
-              icon: icon,
-              iconStyle: style,
-              createdAt: DateTime.fromISO(record.createdAt).toString(),
-              handle: record.operation.handle
-                ? record.operation.handle
-                : record.operation.alsoKnownAs[0],
-              did: record.did,
-              _raw: record.operation,
-            })
-          }
-          this.results = items
-        }
-        } catch (err) {
-          if (isDev()) console.error(err)
-          this.results = err.message
-          if (err.response?.data?.message)
-            this.results = err.response.data.message
-        }
-      },
-    },
+      items.push({
+        id: record.cid,
+        icon: icon,
+        iconStyle: style,
+        createdAt: DateTime.fromISO(record.createdAt).toString(),
+        handle: record.operation.handle
+          ? record.operation.handle
+          : record.operation.alsoKnownAs[0],
+        did: record.did,
+        _raw: record.operation,
+      })
+    }
+    results.value = items
+  }
+  } catch (err) {
+    if (isDev()) console.error(err)
+    results.value = err.message
+    if (err.response?.data?.message)
+      results.value = err.response.data.message
+  }
 }
 </script>
