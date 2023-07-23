5e290e7ab0c6e9f74d37e90b1b94434588363ce6
anon <anon@anoncom.net>
2023-07-23 23:19:43 +0900

    Add link to features in Index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,41 @@
 <template>
   <div
-    class="flex justify-center min-h-screen pt-20 lg:pt-0 sm:items-start lg:items-center bg-gray-100 dark:bg-slate-900">
-    <div class="text-center">
+    class="flex flex-col justify-center max-w-screen-xl min-h-screen pt-10 lg:pt-0 sm:items-start md:items-center bg-gray-100 dark:bg-slate-900">
+    <div class="mx-auto pb-10 text-center">
       <div
         class="text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-400 mb-6">
         {{ appName }}
       </div>
       <div class="text-xl text-gray-700 dark:text-slate-500">
-        Toybox for Bluesky.
+        Toybox for Bluesky
       </div>
+    </div>
+
+    <div class="grid md:grid-cols-2 gap-4 md:gap-8 mx-10 max-w-2xl">
+      <NuxtLink to="/profile" class="feature-card">
+        <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+          View Profile
+        </h2>
+        <p class="text-md font-normal text-gray-500 dark:text-gray-400 mb-4">
+          You can view posts, followings, and other information of users with
+          specified IDs. No login is required becauseas only AT Protocol is
+          used.
+        </p>
+      </NuxtLink>
+      <NuxtLink to="/invite-code" class="feature-card">
+        <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+          Invite code
+        </h2>
+        <span href="#" class="label-auth-required">
+          <font-awesome-icon :icon="['fas', 'lock']" class="mr-1 text-xs" />
+          Authentication required
+        </span>
+        <p class="text-md font-normal text-gray-500 dark:text-gray-400 mb-4">
+          Shows when your invitation code was given, when it was used and by
+          whom. This feature requires you to log in to Bluesky using
+          AppPassword.
+        </p>
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -20,5 +47,10 @@
 </script>
 
 <style scoped>
-  /* スタイル */
+  .feature-card {
+    @apply bg-gray-50 dark:bg-gray-800 hover:bg-gray-200 hover:dark:bg-gray-600 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-6;
+  }
+  .label-auth-required {
+    @apply bg-green-100 text-green-500 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2;
+  }
 </style>
