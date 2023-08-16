d70347034a57cc94c2ef06e7e28d5e3dde1767f0
anon <anon@anoncom.net>
2023-08-17 02:16:50 +0900

    Minor UI changes

--- app/pages/profile/[id].vue
+++ app/pages/profile/[id].vue
@@ -17,7 +17,7 @@
               :disabled="!isLoadingState(loadState)" />
             <label
               for="handle_did"
-              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-translate px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:bg-gray-100 dark:peer-focus:bg-slate-900 left-1">
               Handle or DID
             </label>
           </div>
