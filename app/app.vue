1b706d974f9b1c66823613267747e931939e3b49
anon <anon@anoncom.net>
2023-09-18 01:14:06 +0900

    Remove unused

--- app/app.vue
+++ app/app.vue
@@ -12,10 +12,6 @@
   </div>
 </template>
 
-<script setup lang="ts">
-  import { onMounted } from 'vue'
-
-</script>
 
 <style scoped>
   div {
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = {
   },
   darkMode: 'class',
   plugins: [require('flowbite/plugin')],
+  // modules: ['@nuxtjs/color-mode'],
   colors: {
     gray: colors.gray,
     blue: colors.sky,
