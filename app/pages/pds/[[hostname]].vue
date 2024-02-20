084601632467ef9fa630cd040c2353a0247dfaa2
anon <anon@anoncom.net>
2024-02-21 02:52:55 +0900

    Display atproto PDS info

--- app/pages/pds/[[hostname]].vue
+++ app/pages/pds/[[hostname]].vue
@@ -8,7 +8,7 @@
             v-model="pdsHostname"
             type="text"
             class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder="Enter PDS entry point"
+            placeholder="Enter PDS entry point (e.g. https://bsky.social)"
             @keyup.enter="getPDSInfo" />
         </div>
         <button
