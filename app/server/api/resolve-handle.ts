64096d92974af412d11b250b61cf4a8dfc571bad
anon <anon@anoncom.net>
2024-05-05 01:13:43 +0900

    Update handle DNS resolving API

--- app/server/api/resolve-handle.ts
+++ app/server/api/resolve-handle.ts
@@ -1,23 +1,29 @@
-import { getQuery } from 'h3'
-import dns from 'dns'
+import { defineEventHandler, getQuery } from 'h3'
+import { resolveTxt } from 'node:dns/promises'
 import type { QueryObject } from 'ufo'
-export default defineEventHandler(async (event) => {
+
+export default defineEventHandler(async event => {
   const query: QueryObject = getQuery(event)
   const handle: string = query.handle as string
-  if (!handle)
-    return { error: 'No handle provided' }
-  if (handle.startsWith('did:plc:') || handle.startsWith('did:web:'))
-    return handle
+  if (!handle) return { error: 'No handle provided' }
+  if (handle.startsWith('did:')) return { did: handle }
 
-  try {
-    const records = await dns.promises.resolveTxt(`_atproto.${handle}`)
-    let recordValue: string[] = []
-    for (const i in records)
-      for (const ii in records[i])
-        if (records[i][ii].startsWith('did='))
-          recordValue.push(records[i][ii].startsWith('did=') ? records[i][ii].substring(4) : records[i][ii])
-    return { did: recordValue }
-  } catch (e: any) {
-    return { error: 'No record found', details: { code: e.code, hostname: e.hostname } }
-  }
+  await event.respondWith(requestDNS(handle))
 })
+
+async function requestDNS(handle: string): Promise<Response> {
+  console.log('start resolve via DNS TXT record')
+  const records = await resolveTxt(`_atproto.${handle}`)
+  console.log('records', records)
+  let recordValue: string[] = []
+  for (const i in records)
+    for (const ii in records[i])
+      if (records[i][ii].startsWith('did='))
+        recordValue.push(
+          records[i][ii].startsWith('did=')
+            ? records[i][ii].substring(4)
+            : records[i][ii]
+        )
+  // return recordValue
+  return new Response(JSON.stringify({ did: recordValue }), { status: 200 })
+}
