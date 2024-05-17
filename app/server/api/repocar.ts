d1660c76dbba81fce71f51106411c920d09c6caf
anon <anon@anoncom.net>
2024-05-18 01:39:09 +0900

    Add CAR download API

---
+++ app/server/api/repocar.ts
@@ -0,0 +1,40 @@
+import { defineEventHandler, getQuery } from 'h3'
+import { getPdsEndpoint, getHandle } from '@atproto/common-web'
+import axios from 'axios'
+import type { QueryObject } from 'ufo'
+import { DidResolver } from '@atproto/identity'
+import { DateTime } from 'luxon'
+
+export default defineEventHandler(async event => {
+  const query: QueryObject = getQuery(event)
+  const actor = query.repo as string
+  if (!actor) return { error: 'No DID provided' }
+  if (!actor.startsWith('did:')) return { error: 'Invalid DID' }
+
+  const didResolv = new DidResolver({})
+  const didDoc = await didResolv.resolve(actor)
+  if (!didDoc) return { error: 'No DID document found' }
+
+  const handle = getHandle(didDoc)
+  const pdsEndpoint = getPdsEndpoint(didDoc)
+  if (!pdsEndpoint) return { error: 'No personal data server found' }
+
+  const url = `${pdsEndpoint}/xrpc/com.atproto.sync.getRepo?did=${actor}`
+
+  try {
+    const response = await axios.get(url, { responseType: 'stream' })
+    const date = DateTime.now().toFormat('yyyyMMdd_HHmm')
+    const filename = `${handle}_${date}.car`
+
+    const headers = {
+      'Content-Type': 'application/vnd.ipld.car',
+      'Content-Disposition': `attachment; filename=${filename}`,
+      'Content-Length': response.headers['content-length'],
+      'Cache-Control': 'no-cache',
+    }
+    return new Response(response.data, { status: 200, headers: headers })
+  } catch (error) {
+    console.error(error)
+    return { error: 'Error occurred while downloading file' }
+  }
+})
