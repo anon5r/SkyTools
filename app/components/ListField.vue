4b7dd24db5499c763aa58c674372cbf898047e64
anon <anon@anoncom.net>
2024-07-08 09:53:31 +0900

    Replace placeholder of list avatar

--- app/components/ListField.vue
+++ app/components/ListField.vue
@@ -6,10 +6,26 @@
           class="inline-flex items-center mr-3 text-md font-bold text-gray-900 dark:text-white">
           <!-- Avatar -->
           <fwb-avatar
-            rounded
             :img="avatarURL"
             :alt="props.list.name ?? 'List Avatar'"
-            class="min-w-max avatar-object-cover" />
+            class="min-w-max avatar-object-cover">
+            <template #placeholder>
+              <svg
+                class="w-[42px] h-[42px] text-slate-500 dark:text-slate-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-width="2"
+                  d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5" />
+              </svg>
+            </template>
+          </fwb-avatar>
         </div>
         <div class="xl:max-w-xl lg:max-w-lg md:max-w-md max-w-64 truncate">
           <!-- DisplayName -->
