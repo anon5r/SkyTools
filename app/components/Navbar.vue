a1080745878a1aff413a9c723a65def205ffcbf4
anon <66147+anon5r@users.noreply.github.com>
2026-03-09 16:25:01 +0900

    feat(Navbar): restore toggle dark mode component with easter mode in sidebar

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -137,6 +137,13 @@
                 <span class="ml-3">Sign in Bluesky</span>
               </NuxtLink>
             </li>
+            <li>
+              <div
+                class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <ToggleDarkmode class="flex-shrink-0" />
+                <span class="ml-3">Theme</span>
+              </div>
+            </li>
           </ul>
         </ClientOnly>
       </div>
